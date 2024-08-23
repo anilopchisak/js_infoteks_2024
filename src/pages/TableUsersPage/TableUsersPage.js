48d57dcb85f4f91d7a4d01e33889cd0fcdeddb3e
Polina Kasich <poli.key@yandex.ru>
2024-08-23 11:52:08 +0500

    add: user api

--- src/pages/TableUsersPage/TableUsersPage.js
+++ src/pages/TableUsersPage/TableUsersPage.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
 import './ui/TableUsersPage.scss';
+import {LOADING_STATUS} from "../../app/utils/storeUtils";
+import {Context} from "../../index";
 
 const TableUsersPage = () => {
+
+    const {user} = useContext(Context);
+
+    // получаем данные о пользователях
+    useEffect( () => {
+        // предотвращаем повторные запросы к серверу, если данные уже загружены или находятся в процессе загрузки
+        if ([LOADING_STATUS.SUCCESS, LOADING_STATUS.LOADING].includes(user.userListLoadingStatus)) return;
+
+        const fetchUserList = async () => {
+            try {
+                await user.fetchUserList();
+            }
+            catch (e) {
+                console.log(e.message);
+            }
+        }
+
+        fetchUserList();
+    }, []);
+
     return (
         <div>
 
--- src/pages/TableUsersPage/api/userAPI.js
+++ src/pages/TableUsersPage/api/userAPI.js
@@ -1,7 +1,26 @@
+
 export const fetchUserList = async () => {
-    return true;
+    try {
+        const response = await fetch('https://dummyjson.com/users');
+        const data = await response.json();
+        console.log(data); // Данные для отладки
+        return data;
+    } catch (error) {
+        console.error("Failed to fetch user list:", error);
+        throw error; // Пробрасываем ошибку, чтобы она могла быть обработана
+    }
 }
 
-export const fetchUser = async () => {
-    return true;
+
+
+export const fetchUser = async (id) => {
+    try {
+        const response = await fetch('https://dummyjson.com/users/'+ id);
+        const data = await response.json();
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.error("Failed to fetch user list:", error);
+        throw error;
+    }
 }
